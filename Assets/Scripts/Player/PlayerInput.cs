4eab4b48606bb4614add28ea1160507b2fd47a3c
NeverUNICAL <kesarev0@mail.ru>
2021-10-15 15:06:54 +0300

    v2.2

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -1,20 +1,11 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-[RequireComponent(typeof(PlayerMover))]
-
 public class PlayerInput : MonoBehaviour
 {
     public event UnityAction ClosedGame;
     public event UnityAction ChangedGravity;
-
-    private PlayerMover _mover;
-
-    private void Start()
-    {
-        _mover = GetComponent<PlayerMover>();
-    }
-
+    
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S))
